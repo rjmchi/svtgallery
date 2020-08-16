cee2ff12f38a564db63f7f68ce1c4ffb8509e61d
rjmchi <robert@rjmprogramming.com>
2020-08-16 11:48:24 -0500

    Adds gallery vue component

---
+++ resources/js/components/GalleryComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Gallery</h1>
+        <p>From Vue Component</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1 {color:#5f00f5;}
+</style>
