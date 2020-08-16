5187fce57e302e9350f95667b75c4bac3445cd3b
rjmchi <robert@rjmprogramming.com>
2020-08-16 11:52:33 -0500

    Changes Home to admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in to the admin panel!') }}
                 </div>
             </div>
         </div>
